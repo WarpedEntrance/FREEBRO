eed3933a129d00a4f6ba57d940cd724ceef3654f
SpaceSystem <110506811+WarpedEntrance@users.noreply.github.com>
2025-07-18 21:30:29 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'free ro',
+  description: 'bro',
+  generator: 'vro',
 }
 
 export default function RootLayout({
